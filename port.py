e9fcc5ddcf86943cca42709c897c0691c366eabf
fabian-n-gude-systems <110455261+fabian-n-gude-systems@users.noreply.github.com>
2025-05-23 10:16:11 +0200

    import compatibility, methods static, caching optional

--- port.py
+++ port.py
@@ -19,7 +19,8 @@ args = parser.parse_args()
 
 
 class GudeDevice:
-    def getJson(self, host, ssl, filename, params, username=None, password=None):
+    @staticmethod
+    def getJson(host, ssl, filename, params, username=None, password=None):
         if ssl:
             url = 'https://'
         else:
@@ -32,16 +33,20 @@ class GudeDevice:
             auth = requests.auth.HTTPBasicAuth(username, password)
 
         r = requests.get(url, params=params, verify=False, auth=auth)
-        print (f"{r.url}")
+        print (f"GET {r.url}")
+        print (f"<Response [{r.status_code }]>")
+        print (f"{r.text}")
 
         if r.status_code == 200:
             return json.loads(r.text)
         else:
             raise ValueError("http request error {0}".format(r.status))
 
-    def getPortJson(self, host, ssl, port=None, switch=None, batch=None, states=None, username=None, password=None):
+    @staticmethod
+    def getPortJson(host, ssl=False, port=None, switch=None, batch=None, states=None, username=None, password=None):
         components = 1 + 512
         if port and (switch or batch and states):
+            print(switch)
             if batch and states:
                 params = {'components': components, 'cmd': 5, 'p': port, 's': batch}
                 for idx, state in enumerate(states):
@@ -53,37 +58,46 @@ class GudeDevice:
                 params = {'components': components, 'cmd': 12, 'p': port}
         else:
             params = {'components': components}
-        return self.getJson(host, ssl, 'statusjsn.js', params, username, password)
+        return GudeDevice.getJson(host, ssl, 'statusjsn.js', params, username, password)
 
-    def getPortState(self, port):
+    def getPortState(self, port, use_cache=False):
         if port and port <= self.numPorts:
+            if not use_cache:
+                self.portJson = self.getPortJson(self.host, self.ssl, port, username=self.username, password=self.password)
             return self.portJson["outputs"][port-1]["state"]
         else:
             return None
 
-    def getOvpState(self, ovp):
+    def getOvpState(self, ovp, use_cache=False):
         if ovp and ovp <= self.numBanks:
+            if not use_cache:
+                self.portJson = self.getPortJson(self.host, self.ssl, 1, username=self.username, password=self.password)
             return self.portJson["hardware"]["banks"][ovp-1]["fuse"]
         else:
             return None
 
-    def __init__(self, host, ssl, port=None, switch=None, batch=None, states=None, username=None, password=None):
+    def __init__(self, host, ssl=False, port=None, switch=None, batch=None, states=None, username=None, password=None):
+        self.host = host
+        self.ssl = ssl
+        self.username = username
+        self.password = password
         self.portJson = self.getPortJson(host, ssl, port, switch, batch, states, username, password)
         self.numPorts = len(self.portJson["outputs"])
         self.numBanks = len(self.portJson["hardware"]["banks"])
 
+if __name__ == "__main__":
 
-gudeDevice = GudeDevice(
-    str(args.host), args.ssl,
-    args.port, args.switch,
-    args.batch_delay, args.batch_states,
-    args.username, args.password
-)
+    gudeDevice = GudeDevice(
+        str(args.host), args.ssl,
+        args.port, args.switch,
+        args.batch_delay, args.batch_states,
+        args.username, args.password
+    )
 
-if args.ovp:
-    print(gudeDevice.getOvpState(int(args.ovp)))
-elif args.port:
-    print(gudeDevice.getPortState(int(args.port)))
-else:
-    for p in range(1, gudeDevice.numPorts+1):
-        print(gudeDevice.getPortState(p))
+    if args.ovp:
+        print(gudeDevice.getOvpState(int(args.ovp), use_cache=True))
+    elif args.port:
+        print(gudeDevice.getPortState(int(args.port), use_cache=True))
+    else:
+        for p in range(1, gudeDevice.numPorts+1, use_cache=True):
+            print(gudeDevice.getPortState(p))
